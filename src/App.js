0268ac5e24d34397577d1138655d21dd2376cec0
hubmatyas@gmail.com <mhub@deloittece.com>
2022-04-23 19:49:47 +0200

    Added some frontend stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,196 @@
-import logo from './logo.svg';
+import logo from './res/logo.svg';
+import timeIcon from './res/icons/timeIcon.svg';
+import placeholder from './res/placeholder.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  <div className="content">
+      <div className="container">
+      <nav>
+        <a href="/" className="logo">
+          <img src={logo} alt="logo"/>
         </a>
-      </header>
+        <ul className="navLinks">
+          <li>
+            <a href="/">Přidat recept</a>
+          </li><li>
+            <a href="/">Moje recepty</a>
+          </li><li>
+            <a href="/">Odhlásit se</a>
+          </li>
+          
+        </ul>
+    </nav>
+
+    <header>
+      <div className="headerBox">
+        <h1 className="title">Přispějte do naší databáze<br />řeckých receptů!</h1>
+        <a href="/" className="btn">Přidat recept</a>
+      </div>
+    </header>
+
+    <section className="itemsSection">
+      <h2 className="sectionTitle">Všechny recepty</h2>
+      <div className="listItems">
+        <ul>
+        <li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li><li>
+            <a href="/" className="item">
+              <figure>
+                <img src={placeholder} alt="#" className="thumb" />
+              </figure>
+              <strong className="name">TZATZIKI - originální řecký recept</strong>
+              <div className="meta">
+                <div class="time">
+                  <img src={timeIcon} alt="#" />
+                  <span>20 min</span>
+                </div>
+                <div class="category">
+                  <span>Předkrmy</span>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
     </div>
+    <footer>
+    <p>Aplikace byla vytvořena v&nbsp;rámci předmětu Architektura Cloudových Aplikací</p>
+  </footer>
+
+  </div>
+
+
   );
 }
 
